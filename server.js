c66a0519c4d6330a0522cd2b484ec413766ae30b
mmmiz <miduho4soft@yahoo.co.jp>
2023-11-21 13:30:10 -0600

    server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// server.js
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const path = require('path');  // Import the path module
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,12 +13,25 @@ app.use(cors({
   credentials: true,
 }));
 
+
+app.use(bodyParser.json());
+
+let serverMessage = 'Hello from the server!';
+
+app.get('/api/message', (req, res) => {
+  res.json({ message: serverMessage });
+});
+
+app.post('/api/message', (req, res) => {
+  const { message } = req.body;
+  serverMessage = message;
+  res.json({ message: serverMessage });
+});
+
+
 // Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/api/message', (req, res) => {
-  res.json({ message: 'Hello from the server!' });
-});
 
 // Catch-all route to serve the 'index.html' file
 app.get('*', (req, res) => {
@@ -26,3 +41,4 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
