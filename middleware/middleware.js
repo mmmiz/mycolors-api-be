8e8f7b65ed82011c6092e29be16dd2c85293c4de
mmmiz <miduho4soft@yahoo.co.jp>
2023-12-08 12:02:27 -0600

    updated

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = 'your_secret_key';
+const SECRET_KEY = process.env.SECRET_KEY || 'default_secret_key';
 
 const verifyToken = (req, res, next) => {
   const tokenHeader = req.header('Authorization');
